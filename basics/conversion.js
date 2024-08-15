ce3b1cc4be19770f32c7d9a25fcf3d78a09e424f
Sujal261 <160462343+Sujal261@users.noreply.github.com>
2024-08-15 13:13:03 +0000

    conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,28 +1,45 @@
-let number = 33
-console.log(typeof number);
-let number1 = "33"
-console.log(typeof number1);
-//we can convert this to number by the following step 
-let valueInNumber = Number(number1)// this line converts string to number
-console.log(typeof valueInNumber)
+// let number = 33
+// console.log(typeof number);
+// let number1 = "33"
+// console.log(typeof number1);
+// //we can convert this to number by the following step 
+// let valueInNumber = Number(number1)// this line converts string to number
+// console.log(typeof valueInNumber)
  
-let number3 ="33abc";
-let num = Number(number3);
-console.log(typeof num)
-console.log(num)
-/*
-though it gives the type of number3 as number value it returns is Nan
-*/
-let number4 = null ;
-let num1 = Number(number4);
-console.log(typeof num1)
-console.log(num1)//it gives the value 0
-//it there is boolean value then it return either 1 or 0 
-//for boolean
-let b = 1
-let convert = Boolean(b)
-console.log(convert);
-// 1 => true
-// 0 =>false
-// string=> true
+// let number3 ="33abc";
+// let num = Number(number3);
+// console.log(typeof num)
+// console.log(num)
+// /*
+// though it gives the type of number3 as number value it returns is Nan
+// */
+// let number4 = null ;
+// let num1 = Number(number4);
+// console.log(typeof num1)
+// console.log(num1)//it gives the value 0
+// //it there is boolean value then it return either 1 or 0 
+// //for boolean
+// let b = 1
+// let convert = Boolean(b)
+// console.log(convert);
+// // 1 => true
+// // 0 =>false
+// // string=> true
+
+//operations
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+//  //concatenation
+//  let str1 = "sujal";
+//  let str2 = " Gyawali"
+//  console.log(str1+str2);
+//  console.log("1"+2);
+//  console.log(1+"2");
+ 
+//  console.log("1"+2+2);
+//  console.log(1+2+"2");
+// console.log(+true);// this gives the valllue 1
+ 
+ 
 
