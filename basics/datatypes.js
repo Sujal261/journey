8c5dfff673adef591449da6057ba3752c3662ec4
Sujal261 <160462343+Sujal261@users.noreply.github.com>
2024-08-15 12:31:13 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+//alert(3+3) it wont run because we are using node js not browser
+// code readablility must be high
+let name = "Sujal"
+let age = 18
+let isloggedin =  false
+/*
+number => 2 to power 53
+bigInt
+string => ""
+boolean =>true/false
+null=>standalone value this means we are assiugning empty value
+undefined this means we have defined the variable but we have not assigned
+symbol used for uniqueness
+objects
+*/
+console.log(typeof age)
+console.log(typeof null);// type of null is object and type of undefined is undefined
+console.log(typeof undefined);// type of undefined is undefined
+
+
---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId =144553// this locks the value cannot be changed
+let accountEmail = "hello@gmail.com"
+var accountPassword = "12334"
+hello = "name"
+let accountstate;// if we declare but donot give anyrhing it will be known as undefined
+
+// accountId = 2
+
+accountEmail = "heleee"
+accountPassword = "436645"
+hello = "nepal"
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, hello, accountstate])
+/*
+prefer nno to use in var use let because of issue in block scope and functional scope
+they are accessible outside the blocks
+*/
