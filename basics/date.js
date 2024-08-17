a21bfcf7963373e80ebb13bfd8180692502ae363
Sujal261 <160462343+Sujal261@users.noreply.github.com>
2024-08-17 03:55:42 +0000

    date

---
+++ basics/date.js
@@ -0,0 +1,14 @@
+// // // const date = new Date()
+// // // console.log(date.toDateString());
+// // // console.log(date.toLocaleString());
+// // // console.log(date.toISOString());
+// // // console.log(date.toJSON());
+// // // console.log(typeof date);
+// // let mycreateDate = new Date( 1,22,2023)
+// // console.log(mycreateDate.toLocaleDateString());
+
+// let myCreateDate = new Date("01-14-2023")
+// let myTimestamp = Date.now()
+// console.log(myCreateDate.getTime());
+// console.log(myTimestamp);
+
