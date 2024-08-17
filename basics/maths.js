889129c386dbd4675ee40eb80a650bafb31d3e86
Sujal261 <160462343+Sujal261@users.noreply.github.com>
2024-08-17 03:32:39 +0000

    math

---
+++ basics/maths.js
@@ -0,0 +1,28 @@
+// const score = 400
+// const balance = new Number (100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));// this gives the precision
+// const otherNumber = 23.3422
+// console.log(otherNumber.toPrecision(4));
+// //see its output when necessary 
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))//according to indian standards it gives the value
+
+//////////////////////////////////////////////
+console.log(Math)
+console.log(Math.abs(-4));//same as modulus converts negative to positive
+console.log(Math.round(4.6));
+console.log(Math.floor((Math.random()*10)+1));// this gives the values between alwaysd greater than 
+//math random alaways gives the value between 0 and 1
+const min = 10 
+const max =20 
+console.log(Math.floor(Math.random()*(max - min +1))+min ) 
+
+
+
+
+
+
+
+
