fb73a1fc12c07c9a50c0828c1e2ee50b47ced273
Sujal261 <160462343+Sujal261@users.noreply.github.com>
2024-08-17 03:07:52 +0000

    string.js

---
+++ basics/string.js
@@ -0,0 +1,6 @@
+const name = "Sujal "
+const repoCount ="1"
+console.log(`Heloo  my name is ${name} my repo count is ${repoCount}`);
+const gamename = "Sujal-Gyawali"
+console.log(gamename.split('-'));
+console.log(gamename.length);
